7a11fb972dcdd93c5da19c42e5c036963b538e6d
adrk17 <adrian.krupa17@gmail.com>
2024-01-02 21:22:17 +0100

    generic thickness

--- heat_transfer/generic.py
+++ heat_transfer/generic.py
@@ -3,6 +3,10 @@ class MultiLayerObject:
         self.layers : List[ObjectLayer] = layers  
         self.temperature_1 : float = temperature_1  
         self.temperature_2 : float = temperature_2
+        
+    @property   
+    def thickness(self):
+        return sum(layer.thickness for layer in self.layers)
 
 class Opening:
     def get_surface_area(self):
--- heat_transfer/models/wall.py
+++ heat_transfer/models/wall.py
@@ -9,9 +9,7 @@ class Wall(MultiLayerObject):
         height = height
         openings : List[Opening] = openings
 
-    @property
-    def thickness(self):
-        return sum(layer.thickness for layer in self.layers)
+    
     
     def get_surface_area(self):
         return self.length * self.height - sum(opening.get_surface_area() for opening in self.openings)
