7bd9da6ecb0c1cea9f63e3946f9e8d5776470d5a
adrk17 <adrian.krupa17@gmail.com>
2024-01-02 19:30:03 +0100

    wall object_layer  conductivity

--- heat_transfer/conduction.py
+++ heat_transfer/conduction.py
@@ -1,4 +1,6 @@
 from typing import List, Dict, Tuple, Any
+from .models.wall import *
+
 
 class Conduction:
     def __init__(self, time_step : float):
@@ -6,7 +8,10 @@ class Conduction:
         
     def update_temperature(self, wall : Wall):
         total_conductance = sum(layer.thickness / layer.conductivity for layer in wall.layers)
+        print(total_conductance, 'total_conductance')
         heat_flux_density = -(wall.temperature_1 - wall.temperature_2) / total_conductance
+        print(heat_flux_density, 'heat_flux_density')
         delta_temperature = heat_flux_density * self.time_step / wall.get_total_rho_cp()
+        print(delta_temperature, 'delta_temperature')
         wall.temperature_1 += delta_temperature
         wall.temperature_2 -= delta_temperature
--- runner.py
+++ runner.py
@@ -1,2 +1,17 @@
-from .heat_transfer import conduction
-from .models import wall, object_layer
+from heat_transfer.conduction import *
+from heat_transfer.models.wall import *
+from heat_transfer.models.object_layer import *
+
+
+wall = Wall([ObjectLayer(0.05, 0.026, 780, 500), ObjectLayer(0.1, 0.026, 780, 500)], 1, 1, 20, 100)
+conduction = Conduction(1)
+conduction.update_temperature(wall)
+
+
+# for i in range(10000):
+#     conduction.update_temperature(wall)
+
+print(wall.temperature_1)
+print(wall.temperature_2)
+print(wall.get_thickness())
+print(wall.get_total_rho_cp())
